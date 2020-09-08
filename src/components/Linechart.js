d826cfa415ad469616ce4a9dc1d1c9064a5bf5c4
Panu Aho <panu.aho@wapice.com>
2020-09-08 21:37:41 +0300

    fixed date parsing for linechart

--- src/components/Linechart.js
+++ src/components/Linechart.js
@@ -1,15 +1,31 @@
 import React from 'react'
-import { VictoryChart, VictoryLine, VictoryVoronoiContainer } from 'victory'
+import { VictoryChart, VictoryLine, VictoryVoronoiContainer, VictoryAxis } from 'victory'
+import zip from 'lodash.zip'
 
 const labels = ({ datum }) => (
     `${Math.round(datum.x, 2)}, ${Math.round(datum.y, 2)}`
 )
 
 const Linechart = ({ phones }) => {
+
+    const dateAxis = phones.map(p => {
+        const dd = p.time_stamp.slice(0, 2)
+        const MM = p.time_stamp.slice(3, 5)
+        const yyyy = p.time_stamp.slice(6, 10)
+        const HH = p.time_stamp.slice(11, 13)
+        const mm = p.time_stamp.slice(14)
+        return new Date(yyyy, MM-1, dd, HH, mm)
+    })
+    
+    const prices = phones.map(p => p.price)
+    const data = zip(dateAxis, prices).map(dp => ({x: dp[0], y: dp[1] }))
+    console.log(data)
+
     return (
         <div>
-            <VictoryChart containerComponent={<VictoryVoronoiContainer labels={labels}/>}>
-                <VictoryLine data={phones.map(p => ({x: p.time_stamp, y: p.price }) )}/>
+            <VictoryChart scale={{ x: "time"}} containerComponent={<VictoryVoronoiContainer labels={labels}/>}>
+                <VictoryAxis />
+                <VictoryLine data={data}/>
             </VictoryChart>
         </div>
     )
