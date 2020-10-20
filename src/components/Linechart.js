78087bad73d20dbfd0edc715ad756224d6c71f3c
Panu Aho <panu.aho@wapice.com>
2020-10-20 23:21:34 +0300

    Refactored Linechart to support new date format

--- src/components/Linechart.js
+++ src/components/Linechart.js
@@ -11,14 +11,7 @@ const Linechart = ({ phones }) => {
         return <div>Loading...</div>
     }
     const pricedPhones = phones.filter(p => p.price)
-    const dateAxis = pricedPhones.map(p => {
-        const dd = p.time_stamp.slice(0, 2)
-        const MM = p.time_stamp.slice(3, 5)
-        const yyyy = p.time_stamp.slice(6, 10)
-        const HH = p.time_stamp.slice(11, 13)
-        const mm = p.time_stamp.slice(14)
-        return new Date(yyyy, MM-1, dd, HH, mm)
-    })
+    const dateAxis = pricedPhones.map(p => new Date(p.time_stamp))
 
     const prices = pricedPhones.map(p => p.price)
     const data = zip(dateAxis, prices).map(dp => ({x: dp[0], y: dp[1] }))
